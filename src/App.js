d334c7680305d20fed36ae6feee14d25183c24e7
Shital <shitalcheke10@gmail.com>
2022-04-21 07:27:06 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
 
 function App() {
+const[state,setState]=useState([]);
+const[addArr,setAddArr]=useState([]);
+const onChangeValue=(e)=>
+{
+  console.log("onChange" ,e.target.value);
+  setState(e.target.value);
+
+}
+
+  const submit=(e)=>
+  {
+    e.preventDefault();
+    setAddArr(pre=>[...pre,state]);
+  }
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ 
+    <div>
+        
+   <form onSubmit={submit}>
+     {
+      console.log("addArr" ,addArr)
+       }
+  
+    <input type="text" onChange={onChangeValue}></input>
+    <button>submit</button>
+  </form>
+  <div>
+    <ul>
+    {addArr.map(data=>
+    <li>{data}</li>)}
+    </ul>
+    </div>
+
+       
+       
     </div>
   );
 }
